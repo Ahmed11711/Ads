251d143721fd077331f5a5d8c838736c728469a1
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-10 04:05:56 -0800

    aa

--- app/Http/Controllers/Admin/User/UserController.php
+++ app/Http/Controllers/Admin/User/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Admin\User;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
 use App\Repositories\User\UserRepositoryInterface;
 use App\Http\Controllers\BaseController\BaseController;
 use App\Http\Requests\Admin\User\UserStoreRequest;
@@ -10,17 +13,22 @@ use App\Http\Resources\Admin\User\UserResource;
 
 class UserController extends BaseController
 {
-    public function __construct(UserRepositoryInterface $repository)
-    {
-        parent::__construct();
+ public function __construct(UserRepositoryInterface $repository)
+ {
+  parent::__construct();
 
-        $this->initService(
-            repository: $repository,
-            collectionName: 'User'
-        );
+  $this->initService(
+   repository: $repository,
+   collectionName: 'User'
+  );
 
-        $this->storeRequestClass = UserStoreRequest::class;
-        $this->updateRequestClass = UserUpdateRequest::class;
-        $this->resourceClass = UserResource::class;
-    }
+  $this->storeRequestClass = UserStoreRequest::class;
+  $this->updateRequestClass = UserUpdateRequest::class;
+  $this->resourceClass = UserResource::class;
+ }
+
+ public function update(Request $request, int $id): JsonResponse
+ {
+  return parent::update($request, $id);
+ }
 }
