c87858dd81222417ea439255fa07da5c6ba9a745
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-10 04:36:20 -0800

    aa

---
+++ app/Http/Controllers/heleperController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class heleperController extends Controller
+{
+ public function notification(Request $request)
+ {
+  return 555;
+ }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -1,31 +1,30 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\setting\settingController;
+use App\Http\Controllers\Admin\Auth\AuthController;
 use App\Http\Controllers\Admin\Company\CompanyController;
 use App\Http\Controllers\Admin\notifications\notificationsController;
-use App\Http\Controllers\Admin\withdraw\withdrawController;
-use App\Http\Middleware\CheckJwtTokenByAdmin;
-use App\Http\Controllers\Admin\Auth\AuthController;
+use App\Http\Controllers\Admin\setting\settingController;
 use App\Http\Controllers\Admin\User\UserController;
+use App\Http\Controllers\Admin\withdraw\withdrawController;
+use App\Http\Controllers\heleperController;
+use App\Http\Middleware\CheckJwtTokenByAdmin;
 use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
 
 Route::post('admin/v1/login', [AuthController::class, 'login'])->name('admin.login');
 Route::prefix('admin/v1')->middleware(CheckJwtTokenByAdmin::class)->group(function () {
-    Route::apiResource('users', UserController::class)->names('user');
-    Route::apiResource('withdraws', withdrawController::class)->names('withdraw');
-    Route::apiResource('notifications', notificationsController::class)->names('notifications');
-    Route::get('my-affiliate', [AuthController::class, 'myAffiliate']);
-    Route::apiResource('companies', CompanyController::class)->names('company');
-    Route::apiResource('settings', settingController::class)->names('setting');
-    Route::get('all-emails',function(){
-        return User::select(['id','email'])->get();
-    });
-
-
+ Route::apiResource('users', UserController::class)->names('user');
+ Route::apiResource('withdraws', withdrawController::class)->names('withdraw');
+ Route::apiResource('notifications', notificationsController::class)->names('notifications')->except('post');
+ Route::post('notifications', [heleperController::class, 'notification']);
+ Route::get('my-affiliate', [AuthController::class, 'myAffiliate']);
+ Route::apiResource('companies', CompanyController::class)->names('company');
+ Route::apiResource('settings', settingController::class)->names('setting');
+ Route::get('all-emails', function () {
+  return User::select(['id', 'email'])->get();
+ });
 });
 
 
-Route::prefix('v1')->group(function () {
-    
-});
+Route::prefix('v1')->group(function () {});
