d324244ddbebf170b0f8d5c88e7853adbcb3d161
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-10 04:40:52 -0800

    aa

--- app/Http/Controllers/heleperController.php
+++ app/Http/Controllers/heleperController.php
@@ -10,6 +10,18 @@ class heleperController extends Controller
  public function notification(notificationsStoreRequest $request)
  {
   $data = $request->validated();
+
+  // تحويل emails من string مفصول بفواصل إلى array
+  if (isset($data['emails']) && is_string($data['emails'])) {
+   // إزالة الأقواس المربعة لو موجودة
+   $emails = trim($data['emails'], '[]');
+   // تقسيم على الفاصلة وتنظيف الفراغات
+   $emailsArray = array_map('trim', explode(',', $emails));
+   $data['emails'] = $emailsArray;
+  }
+
+  // لو emails أصلاً array، يبقى يفضل كما هو
+  // الآن $data['emails'] جاهز كـ array
   return $data;
  }
 }
