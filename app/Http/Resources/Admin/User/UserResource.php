140837d7435599f190bcf5dc037c3a846ec7acde
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-11 08:44:01 -0800

    aa

--- app/Http/Resources/Admin/User/UserResource.php
+++ app/Http/Resources/Admin/User/UserResource.php
@@ -23,8 +23,9 @@ class UserResource extends JsonResource
    'address' => $this->address,
    'profile_image' => $this->profile_image,
    'role' => $this->role,
-   'balance' => 0,
-   'balance_affiliate' =>  0,
+   'balance' => optional($this->balance)->balance ?? 0,
+   'balance_affiliate' => optional($this->balance)->affiliate_balance ?? 0,
+
    'created_at' => $this->created_at,
    'updated_at' => $this->updated_at,
   ];
