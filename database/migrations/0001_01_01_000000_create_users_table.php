4a2380d0deb5ad57bd7fd0b0df6c523633fa47a9
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-13 00:44:18 -0800

    aaa

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -26,6 +26,12 @@ return new class extends Migration
    $table->string('address')->nullable();
    $table->enum('role', ['admin', 'user'])->default('user');
    $table->text('fcm_token')->nullable();
+   $table->timestamp('last_login_at')->nullable();
+
+   $table->string('bank_name')->nullable();
+   $table->string('iban')->nullable();
+   $table->string('country')->nullable();
+   $table->string('wallet')->nullable();
 
    $table->rememberToken();
    $table->timestamps();
