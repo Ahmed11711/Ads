a38c7a21accd4815ce27e44cd0a4f3debc83e8fe
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-14 11:16:53 -0800

    aaa

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\User\UpdateUserController;
 use App\Http\Controllers\Api\Ads\MyHistroryController;
 use App\Http\Controllers\Api\Affiliate\AffiliateController;
 use App\Http\Controllers\Api\Auth\AuthController;
@@ -10,8 +9,9 @@ use App\Http\Controllers\Api\Notifications\NotificationsController;
 use App\Http\Controllers\Api\Withdraw\WithdrawController;
 use App\Http\Middleware\CheckJwtToken;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use App\Http\Controllers\Admin\User\UpdateUserController;
 
 
 
@@ -47,68 +47,58 @@ Route::prefix('v1/')->group(function () {
   return 55545455555555;
  });
  Route::post('seocil-login', [AuthController::class, 'socailLogin']);
- Route::get('/run-storage-link', function () {
-  try {
-   Artisan::call('storage:link');
+ Route::get('/run-migrate', function () {
+  Artisan::call('migrate', [
+   // '--seed' => true, // لو عايز يشغل الـ seeders كمان
+   '--force' => true, // لتأكيد التنفيذ على بيئة الإنتاج
+  ]);
 
-   return response()->json([
-    'success' => true,
-    'message' => 'Storage link created successfully',
-   ]);
-  } catch (\Exception $e) {
-   return response()->json([
-    'success' => false,
-    'message' => 'Failed to create storage link: ' . $e->getMessage(),
-   ]);
-  }
+
+  return response()->json([
+   'success' => true,
+   'message' => 'Migration ran successfully',
+  ]);
  });
+ // Route::get('/run-storage-link', function () {
+
+ //  // لو اللينك موجود قبل كده، امسحه
+ //  if (File::exists(public_path('storage'))) {
+ //   File::delete(public_path('storage'));
+ //  }
+
+ //  Artisan::call('storage:link');
+
+ //  return response()->json([
+ //   'status' => true,
+ //   'message' => 'Storage link created successfully',
+ //  ]);
+ // });
+
+ // Route::get('/run-seeder', function () {
+ //  Artisan::call('db:seed', [
+ //   '--force' => true,
+ //  ]);
+ //  return 'Database seeding completed!';
+ // });
+ Route::get('settings', [CompanyCOntroller::class, 'setting']);
 
 
- return response()->json([
-  'success' => true,
-  'message' => 'Migration ran successfully',
- ]);
+ Route::middleware(CheckJwtToken::class)->group(function () {
+  Route::get('withdraw', [WithdrawController::class, 'index']);
+  Route::get('my-balance', [AuthController::class, 'getBalance']);
+
+  // without auth routes
+  Route::get('my-affiliate', [AffiliateController::class, 'myAffiliate']);
+  Route::get('notification', [NotificationsController::class, 'index']);
+
+  Route::post('withdraw', [WithdrawController::class, 'Withdraw']);
+  Route::post('add-balance', [WithdrawController::class, 'addBalance']);
+  Route::get('companies', [CompanyCOntroller::class, 'index']);
+  Route::post('/send-notification', [NotificationController::class, 'send']);
+  Route::post('/read-all-notification', [NotificationController::class, 'readAll']);
+  Route::post('see-ads', [CompanyCOntroller::class, 'userAds']);
+  Route::get('my-history-ads', [MyHistroryController::class, 'index']);
+ });
 });
-// Route::get('/run-storage-link', function () {
-
-//  // لو اللينك موجود قبل كده، امسحه
-//  if (File::exists(public_path('storage'))) {
-//   File::delete(public_path('storage'));
-//  }
-
-//  Artisan::call('storage:link');
-
-//  return response()->json([
-//   'status' => true,
-//   'message' => 'Storage link created successfully',
-//  ]);
-// });
-
-// Route::get('/run-seeder', function () {
-//  Artisan::call('db:seed', [
-//   '--force' => true,
-//  ]);
-//  return 'Database seeding completed!';
-// });
-Route::get('settings', [CompanyCOntroller::class, 'setting']);
-
-
-Route::middleware(CheckJwtToken::class)->group(function () {
- Route::get('withdraw', [WithdrawController::class, 'index']);
- Route::get('my-balance', [AuthController::class, 'getBalance']);
-
- // without auth routes
- Route::get('my-affiliate', [AffiliateController::class, 'myAffiliate']);
- Route::get('notification', [NotificationsController::class, 'index']);
-
- Route::post('withdraw', [WithdrawController::class, 'Withdraw']);
- Route::post('add-balance', [WithdrawController::class, 'addBalance']);
- Route::get('companies', [CompanyCOntroller::class, 'index']);
- Route::post('/send-notification', [NotificationController::class, 'send']);
- Route::post('/read-all-notification', [NotificationController::class, 'readAll']);
- Route::post('see-ads', [CompanyCOntroller::class, 'userAds']);
- Route::get('my-history-ads', [MyHistroryController::class, 'index']);
-});
-
 
 require __DIR__ . '/admin.php';
