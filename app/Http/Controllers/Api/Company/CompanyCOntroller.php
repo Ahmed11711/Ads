51209420346eb8411af4e1ea5a977fc95384ebea
Ahmed11711 <ahmed11711@gmail.com>
2025-12-24 02:35:38 +0200

    as

--- app/Http/Controllers/Api/Company/CompanyCOntroller.php
+++ app/Http/Controllers/Api/Company/CompanyCOntroller.php
@@ -7,62 +7,82 @@ use App\Http\Requests\UserAdsRequest;
 use App\Http\Resources\myAdsResource;
 use App\Models\Company;
 use App\Models\setting;
+use App\Models\userBalance;
 use App\Models\userWithAds;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CompanyCOntroller extends Controller
 {
- use ApiResponseTrait;
- public function index(Request $request)
- {
-  $query = Company::where('status', 'active');
+    use ApiResponseTrait;
+    public function index(Request $request)
+    {
+        $query = Company::where('status', 'active');
 
-  if ($request->filled('type')) {
-   $query->where('type', $request->type);
-  } else {
-   $query->where('type', '!=', 'tasks');
-  }
+        if ($request->filled('type')) {
+            $query->where('type', $request->type);
+        } else {
+            $query->where('type', '!=', 'tasks');
+        }
 
-  $companies = $query->get();
+        $companies = $query->get();
 
-  return $this->successResponse(
-   $companies,
-   'Companies retrieved successfully',
-   200
-  );
- }
+        return $this->successResponse(
+            $companies,
+            'Companies retrieved successfully',
+            200
+        );
+    }
 
 
 
- public function setting()
- {
-  $settings = setting::get();
-  $settingsObject = $settings->pluck('value', 'key')->toArray();
+    public function setting()
+    {
+        $settings = setting::get();
+        $settingsObject = $settings->pluck('value', 'key')->toArray();
 
-  return $this->successResponse($settingsObject, 'All settings', 200);
- }
- public function userAds(UserAdsRequest $request)
- {
-  $user = auth()->user();
-  $data = $request->validated();
+        return $this->successResponse($settingsObject, 'All settings', 200);
+    }
+    public function userAds(UserAdsRequest $request)
+    {
+        $user = auth()->user();
+        $data = $request->validated();
 
-  $company = Company::findOrFail($data['company_id']);
-  $priceAds = setting::where('key', 'price_ads')->value('value');
+        $company = Company::findOrFail($data['company_id']);
 
-  $userAds = UserWithAds::create([
-   'user_id'    => $user->id,
-   'company_id' => $company->id,
-   'amount'     => $priceAds,      // القيمة المخزنة في settings
-   'status'     => 'pending',
-   'is_active'  => true,
-   'type'       => $company->type,
-  ]);
+        $priceAds = setting::where('key', 'price_ads')->value('value');
 
-  return $this->successResponse(
-   new MyAdsResource($userAds),
-   'Ad created successfully',
-   201
-  );
- }
+        $userAds = UserWithAds::create([
+            'user_id'    => $user->id,
+            'company_id' => $company->id,
+            'amount'     => $priceAds,
+            'status'     => 'pending',
+            'is_active'  => true,
+            'type'       => $company->type,
+        ]);
+
+        if (!empty($data['company_profit'])) {
+            // تحديث رصيد الشركة
+            $company->amount += $data['company_profit'];
+            $company->save();
+
+            // تحديث حالة الإعلان
+            $userAds->status = "complete";
+            $userAds->save();
+
+
+            // updateOrCreate على user balance
+            userBalance::updateOrCreate(
+                ['user_id' => $user->id],
+                ['balance' => DB::raw("balance + $priceAds")]
+            );
+        }
+
+        return $this->successResponse(
+            new MyAdsResource($userAds),
+            'Ad created successfully',
+            201
+        );
+    }
 }
--- app/Http/Requests/UserAdsRequest.php
+++ app/Http/Requests/UserAdsRequest.php
@@ -7,19 +7,20 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UserAdsRequest extends BaseRequest
 {
- /**
-  * Determine if the user is authorized to make this request.
-  */
+    /**
+     * Determine if the user is authorized to make this request.
+     */
 
- /**
-  * Get the validation rules that apply to the request.
-  *
-  * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-  */
- public function rules(): array
- {
-  return [
-   'company_id' => 'required|string|exists:companies,id',
-  ];
- }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'company_id' => 'required|string|exists:companies,id',
+            'company_profit' => 'nullable|numeric',
+        ];
+    }
 }
